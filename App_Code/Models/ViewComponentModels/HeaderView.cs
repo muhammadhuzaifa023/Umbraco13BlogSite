60e7579213b18e27938ae31b348888fbc336b87d
Muhammad Huzaifa <64376908+muhammadhuzaifa023@users.noreply.github.com>
2024-04-02 15:22:08 +0500

    Add project files.

---
+++ App_Code/Models/ViewComponentModels/HeaderView.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbraco13BlogSite.App_Code.Models.ViewComponentModels
+{
+    public class HeaderView
+    {
+        public string? Title { get; set; }
+        public string? SubTitle { get; set; }
+        public string? Imageurl { get; set; }
+
+
+
+    }
+}
---
+++ App_Code/Models/ViewComponentModels/MetaDataView.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbraco13BlogSite.App_Code.Models.ViewComponentModels
+{
+    public class MetaDataView
+    {
+        public string? Title { get; set; }
+        public string?  Description  { get; set; }
+        public string? Author { get; set; }
+
+
+
+    }
+}
