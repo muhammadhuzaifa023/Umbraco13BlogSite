e803cd462ae468f9ae5f8011290cd42f2744ffec
Muhammad Huzaifa <64376908+muhammadhuzaifa023@users.noreply.github.com>
2024-04-04 17:27:55 +0500

    updated

---
+++ App_Code/Models/ViewComponentModels/BlogView.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbraco13BlogSite.App_Code.Models.ViewComponentModels
+{
+    public class BlogView
+    {
+        public string? Title { get; set; }
+        public string? SubTitle { get; set; }
+        public string? BlogUrl { get; set; }
+        public string? CreatedBy { get; set; }
+        //public string? AuthorUrl { get; set; }
+        public string? CreatedDate { get; set; }
+    }
+}
---
+++ App_Code/ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Umbraco.Cms.Web.Common;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using Umbraco13BlogSite.App_Code.Models.ViewComponentModels;
+
+namespace Umbraco13BlogSite.App_Code.ViewComponents
+{
+    [ViewComponent]
+    public class BlogViewComponent: ViewComponent
+    {
+        private readonly ILogger<BlogViewComponent> _logger;
+        private readonly UmbracoHelper _umbracoHelper;
+
+        public BlogViewComponent(ILogger<BlogViewComponent> logger, UmbracoHelper umbracoHelper)
+        {
+            _logger = logger;
+            _umbracoHelper = umbracoHelper;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            List<BlogView> blogView = new();
+            try
+            {
+                var content = _umbracoHelper?.ContentAtRoot()?
+                    .FirstOrDefault(x => x.IsDocumentType("home"))?
+                    .Children()?
+                    .FirstOrDefault(x => x.IsVisible()
+                    && x.IsDocumentType("blog")) as Blog;
+
+                if (content == null) return View(blogView);
+
+                foreach (Blog item in content?.Children() ?? new List<Blog>())
+                {
+                    blogView?.Add(new BlogView
+                    {
+                        Title = item?.Name,
+                        SubTitle = item?.SubTitle,
+                        BlogUrl = item?.Url(),
+                        CreatedBy = item?.CreatorName(),
+                        //AuthorUrl = item?.AuthorPageUrl?.Url(),
+                        CreatedDate = item?.CreateDate.ToString("D")
+                    });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error While Processing {nameof(BlogViewComponent)}");
+            }
+
+
+            return View(blogView);
+        }
+
+
+    }
+}
